fe0390a97b48dded44ffc4e8442df92e3b637069
迷渡 <justjavac@gmail.com>
2026-06-28 22:18:48 +0800

    chore(core): remove obsolete bridge templates

--- proton/core/assets/moonbit_register_op.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  const moonbit = window[__MOONBIT_NAME_JS__];
-  const opName = __OP_NAME_JS__;
-  if (moonbit && moonbit.core && typeof moonbit.core.registerOp === 'function') {
-    moonbit.core.registerOp(opName);
-  }
-})();
