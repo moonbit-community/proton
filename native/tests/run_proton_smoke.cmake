a44035b05cfcaf2a7e72c4d9dacc8ae89f3461a0
justjavac <justjavac@gmail.com>
2026-07-28 02:29:44 +0800

    test(native): disable temporary CEF logging

--- native/tests/run_proton_smoke.cmake
+++ native/tests/run_proton_smoke.cmake
@@ -3,10 +3,6 @@ if(NOT DEFINED PROTON_SMOKE_EXECUTABLE OR
   message(FATAL_ERROR "PROTON_SMOKE_EXECUTABLE is required")
 endif()
 
-if(DEFINED PROTON_CEF_LOG_FILE AND NOT PROTON_CEF_LOG_FILE STREQUAL "")
-  file(REMOVE "${PROTON_CEF_LOG_FILE}")
-endif()
-
 execute_process(
   COMMAND "${PROTON_SMOKE_EXECUTABLE}"
   RESULT_VARIABLE result
@@ -18,18 +14,10 @@ if(NOT "${result}" STREQUAL "0")
      EXISTS "$ENV{PROTON_TEST_NATIVE_LOG}")
     file(READ "$ENV{PROTON_TEST_NATIVE_LOG}" native_log)
   endif()
-  set(cef_log "")
-  if(DEFINED PROTON_CEF_LOG_FILE AND
-     EXISTS "${PROTON_CEF_LOG_FILE}")
-    file(READ "${PROTON_CEF_LOG_FILE}" cef_log)
-  endif()
   message(FATAL_ERROR
     "proton_smoke failed: ${result}\n"
     "--- Proton native log ---\n"
     "${native_log}\n"
-    "--- End Proton native log ---\n"
-    "--- CEF debug log ---\n"
-    "${cef_log}\n"
-    "--- End CEF debug log ---"
+    "--- End Proton native log ---"
   )
 endif()
