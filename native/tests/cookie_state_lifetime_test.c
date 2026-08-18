c964eb302a1eb323354e56182b302d08b1b79983
justjavac <justjavac@gmail.com>
2026-08-18 20:47:36 +0800

    test(native): keep cookie lifetime checks active in release

--- native/tests/cookie_state_lifetime_test.c
+++ native/tests/cookie_state_lifetime_test.c
@@ -1,6 +1,12 @@
 #include "../src/engine/cef_common/cookie_state_lifetime.h"
 
-#include <assert.h>
+#include <stdlib.h>
+
+static void require(int condition) {
+  if (!condition) {
+    abort();
+  }
+}
 
 static void test_cleanup_without_visitor_releases_state(void) {
   proton_cookie_state_ref_count_t refs;
@@ -9,8 +15,9 @@ static void test_cleanup_without_visitor_releases_state(void) {
   proton_cookie_state_lifetime_init(&refs, &detached);
   proton_cookie_state_lifetime_detach(&detached);
 
-  assert(proton_cookie_state_lifetime_is_detached(&detached));
-  assert(proton_cookie_state_lifetime_release(&refs));
+  require(proton_cookie_state_lifetime_is_detached(&detached));
+  int last = proton_cookie_state_lifetime_release(&refs);
+  require(last);
 }
 
 static void test_cleanup_waits_for_visitor_release(void) {
@@ -21,9 +28,11 @@ static void test_cleanup_waits_for_visitor_release(void) {
   proton_cookie_state_lifetime_retain(&refs);
   proton_cookie_state_lifetime_detach(&detached);
 
-  assert(proton_cookie_state_lifetime_is_detached(&detached));
-  assert(!proton_cookie_state_lifetime_release(&refs));
-  assert(proton_cookie_state_lifetime_release(&refs));
+  require(proton_cookie_state_lifetime_is_detached(&detached));
+  int owner_last = proton_cookie_state_lifetime_release(&refs);
+  require(!owner_last);
+  int visitor_last = proton_cookie_state_lifetime_release(&refs);
+  require(visitor_last);
 }
 
 static void test_completed_visitor_releases_before_cleanup(void) {
@@ -33,10 +42,12 @@ static void test_completed_visitor_releases_before_cleanup(void) {
   proton_cookie_state_lifetime_init(&refs, &detached);
   proton_cookie_state_lifetime_retain(&refs);
 
-  assert(!proton_cookie_state_lifetime_release(&refs));
-  assert(!proton_cookie_state_lifetime_is_detached(&detached));
+  int visitor_last = proton_cookie_state_lifetime_release(&refs);
+  require(!visitor_last);
+  require(!proton_cookie_state_lifetime_is_detached(&detached));
   proton_cookie_state_lifetime_detach(&detached);
-  assert(proton_cookie_state_lifetime_release(&refs));
+  int owner_last = proton_cookie_state_lifetime_release(&refs);
+  require(owner_last);
 }
 
 int main(void) {
